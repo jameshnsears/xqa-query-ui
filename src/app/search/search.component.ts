86f7303a3f6a3bf8f7739ead34405188809a6074
James Sears <james.hn.sears@gmail.com>
2018-03-22 15:11:45 +0000

    wip

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -10,17 +10,24 @@ import { SearchService } from '../service/search.service';
 })
 export class SearchComponent implements OnInit {
   searchValue: string;
-
   searchResponses: SearchResponse[];
 
+  error: any;
+
   constructor(private searchService: SearchService) {
+    this.searchValue = '';
   }
 
   search() {
+    this.searchService.getConfig(this.searchValue)
+    .then(
+      searchResponses => this.searchResponses = searchResponses,
+      error => this.error = error
+    );
   }
 
   ngOnInit() {
-    this.searchService.getSearchResponse().then(searchResponses => this.searchResponses = searchResponses);
+    //this.searchService.getSearchResponse(this.searchValue).then(searchResponses => this.searchResponses = searchResponses);
   }
 
 }
--- src/app/service/search.service.ts
+++ src/app/service/search.service.ts
@@ -3,17 +3,45 @@ import 'rxjs/add/operator/toPromise';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { catchError, retry } from 'rxjs/operators';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+import { HttpParams, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+
 import { SearchResponse } from '../domain/search.response';
+
 
 @Injectable()
 export class SearchService {
 
   constructor(private http: HttpClient) { }
 
-  getSearchResponse() {
-    return this.http.get<any>('assets/search.response.json')
+  getConfig(searchValue: string) {
+    return this.http.get<any>('assets/search.response.json',
+      { params: new HttpParams().set('searchValue', searchValue) })
       .toPromise()
       .then(res => <SearchResponse[]>res.data)
       .then(data => data);
+  }
+
+  getHeroes(searchValue: string): Observable<SearchResponse[]> {
+    return this.http.get<SearchResponse[]>('assets/search.response.json',
+    { params: new HttpParams().set('searchValue', searchValue) })
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return new ErrorObservable(
+      'Something bad happened; please try again later.');
   }
 }
