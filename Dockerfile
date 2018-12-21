da69e9c3152fd2a7c1e48e22e6f82217953c8a5c
James Sears <james.hn.sears@gmail.com>
2018-12-21 19:51:37 +0000

    codacy

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM nginx:1.12.2-alpine
 
-MAINTAINER james.hn.sears@gmail.com
-
 COPY dist /usr/share/nginx/html
 
 EXPOSE 80
--- karma.conf.js
+++ karma.conf.js
@@ -3,28 +3,28 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-sonarqube-reporter')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+      require("karma-sonarqube-reporter")
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+      dir: require("path").join(__dirname, "coverage"), reports: [ "html", "lcovonly" ],
       fixWebpackSourcePaths: true
     },
     sonarqubeReporter: {
-      basePath: 'src/app',        // test files folder
-      filePattern: '**/*spec.ts', // test files glob pattern
-      encoding: 'utf-8',          // test files encoding
-      outputFolder: 'reports',    // report destination
+      basePath: "src/app",        // test files folder
+      filePattern: "**/*spec.ts", // test files glob pattern
+      encoding: "utf-8",          // test files encoding
+      outputFolder: "reports",    // report destination
       legacyMode: false,          // report for Sonarqube < 6.2 (disabled)
       reportName: (metadata) => { // report name callback
         /**
@@ -34,16 +34,16 @@ module.exports = function (config) {
          * - metadata[2] = plataform name
          * - metadata[3] = plataform version
          */
-         return metadata.concat('xml').join('.');
+         return metadata.concat("xml").join(".");
       },
     },
 
-    reporters: ['progress', 'kjhtml', 'sonarqube'],
+    reporters: ["progress", "kjhtml", "sonarqube"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ["Chrome"],
     singleRun: false
   });
 };
